ba92711d72c1ac7d0db56aa1d6c3ed0cf9cd2473
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2023-11-11 16:47:57 +0300

    Day3

--- Mathematics/Program.cs
+++ Mathematics/Program.cs
@@ -4,4 +4,4 @@ FourOperation fourOperation = new FourOperation();
 fourOperation.Addition(10, 20);
 fourOperation.Subtraction(10, 20);
 fourOperation.Multiplication(10, 20);
-fourOperation.Division(10, 20);
+fourOperation.Division(10, 20); 
